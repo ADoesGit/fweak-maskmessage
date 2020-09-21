9732b9dd06199b6eef2887ae1aa36bb6efab415a
fweak-2 <71653631+fweak-2@users.noreply.github.com>
2020-09-21 10:18:41 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const { messages, channels } = require('powercord/webpack');
+const { Plugin } = require('powercord/entities');
+
+module.exports = class MaskMessage extends Plugin {
+    async startPlugin() {
+        powercord.api.commands.registerCommand({
+            command: 'mask',
+            aliases: ['mask-message'],
+            description: 'Masks a message with another message!',
+            usage: '{c} <overview> | <masked item>',
+            category: 'fun',
+            executor: await this.commandExec.bind(this)
+        });
+    }
+
+    async commandExec(args) {
+        const string = args.join(' ');
+        const split = string.split(' | ');
+
+        try {
+            await messages.sendMessage(channels.getChannelId(), { content: `${split[0]} ${' _'.repeat(399)} ${split[1]}` });
+        } catch (err) {
+            console.error(`[MaskMessage] -> ${err}`);
+            return;
+        }
+    }
+
+    pluginWillUnload() {
+        powercord.api.commands.unregisterCommand('mask');
+    }
+}
